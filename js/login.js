af35d82a7cba0266b9c5f31b9e393d2b1052b82d
unknown <jossijosi86@gmail.com>
2024-12-23 21:03:35 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,9 @@
+// console.log('added extra btn response')
+
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault();
+    console.log('login button clicked');
+
+    const phoneNumber =document.getElementById('phone-number').value
+    console.log(phoneNumber)
+})
