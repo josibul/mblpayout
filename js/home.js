81a94595a2c5ff07ec4f751bf0be28ab3ea9c96a
unknown <jossijosi86@gmail.com>
2024-12-24 10:35:35 +0600

    added add money feature

--- js/home.js
+++ js/home.js
@@ -10,7 +10,22 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     const inputAmount = document.getElementById('input-amount').value;
     console.log(inputAmount);
     const inputPin = document.getElementById('input-pin').value;
-    console.log(inputPin)
+    console.log(inputPin);
+
+    if(inputPin==="1234"){
+        console.log('added money to acount')
+        const inputAmount = document.getElementById('input-amount').value;
+        const newBalance = document.getElementById('balance').innerText;
+
+        const inputAmt =parseFloat(inputAmount);
+        const newamt=parseFloat(newBalance);
+        const currentBalance = inputAmt+ newamt;
+        console.log(currentBalance,newamt,inputAmt)
+        document.getElementById('balance').innerText=currentBalance;
+    }
+    else{
+        alert('try again')
+    }
 
 
 
